e1b5abaa2b5b8170b3a1d56160bd0fcf43361b39
xondamirprogrammer <132694909+xondamirprogrammer@users.noreply.github.com>
2025-08-15 00:24:53 +0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ export default {
     './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1400px',
+      },
+    },
     extend: {
       borderRadius: {
         lg: 'var(--radius)',
